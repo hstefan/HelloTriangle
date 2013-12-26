145f8af79f981a139571ae7a1a40d175df7a63dc
Hugo Puhlmann <hugopuhlmann@gmail.com>
2013-12-26 15:28:13 -0200

    Small modification on our fragment shader (now we have a lerped color for our fragments)

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -4,5 +4,6 @@ out vec4 outputColor;
 
 void main()
 {
-	outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	outputColor = mix(vec4(1.0f, 1.0f, 1.0f, 1.0f),
+		vec4(0.2f, 0.2f, 0.2f, 1.0f), gl_FragCoord.y/500.0f);
 }
